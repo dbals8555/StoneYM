594687c792775298001cfa8fedf47dfeaacfce29
송유민 <dbals8555@dddd>
2018-02-26 09:50:17 +0900

    initial commit

---
+++ src/main/java/net/sym/SymApplication.java
@@ -0,0 +1,12 @@
+package net.sym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymApplication.class, args);
+	}
+}
---
+++ src/test/java/net/sym/SymApplicationTests.java
@@ -0,0 +1,16 @@
+package net.sym;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SymApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
