2a914f382c81252c633b324db976695bbc187ba2
송유민 <dbals8555@dddd>
2018-03-05 13:31:24 +0900

    pratice

---
+++ src/main/java/net/sym/web/User.java
@@ -0,0 +1,24 @@
+package net.sym.web;
+
+public class User {
+	private String userId;
+	private String password;
+	private String name;
+	private String email;
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
