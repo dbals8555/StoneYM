e07a4def7a8b7b6e5a08e0fba634c8624528a6c0
송유민 <dbals8555@dddd>
2018-03-06 10:36:09 +0900

    질문하기 등록 구현

--- src/main/java/net/sym/web/HomeController.java
+++ src/main/java/net/sym/web/HomeController.java
@@ -1,12 +1,20 @@
 package net.sym.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import net.sym.domain.QuestionRepository;
 
 @Controller
 public class HomeController {
+	@Autowired
+	private QuestionRepository questionsRepository;
+	
 	@GetMapping("")
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("questions", questionsRepository.findAll());
 		return "index";
 	}
 }
